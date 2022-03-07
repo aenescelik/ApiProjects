172d3a33732017178bfe68e8b76cd615b90f087f
aenescelik <enescelik2022@gmail.com>
2022-03-08 02:08:27 +0300

    day 09 end

--- Batch30ApiProject/src/test/java/com/techproed/day08/GetRequest13.java
+++ Batch30ApiProject/src/test/java/com/techproed/day08/GetRequest13.java
@@ -4,9 +4,11 @@ import com.techproed.testBase.DummyTestBase;
 import com.techproed.testData.DummyTestData;
 import groovyjarjarantlr4.runtime.ANTLRReaderStream;
 import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.HashMap;
+import java.rmi.MarshalledObject;
+import java.util.*;
 
 import static io.restassured.RestAssured.given;
 
@@ -31,15 +33,69 @@ public class GetRequest13 extends DummyTestBase {
     @Test
     public void test() {
 
-        spec03.pathParam("parametre1","employees");
+        spec03.pathParam("parametre1", "employees");
 
         DummyTestData expectedObje = new DummyTestData();
-        HashMap<String,Object> expectedDataMap = expectedObje.setUpTestData();
+        HashMap<String, Object> expectedDataMap = expectedObje.setUpTestData();
         System.out.println(expectedDataMap);
 
         Response response = given().accept("application/json")
                 .spec(spec03).when().get("/{parametre1}");
-        response.prettyPrint();
+
+        //response.prettyPrint();
+
+        //deserialization
+        HashMap<String, Object> actualDataMap = response.as(HashMap.class);
+        System.out.println(actualDataMap);
+
+        //Status kodun 200 olduğunu,
+        Assert.assertEquals(expectedDataMap.get("statusCode"), response.getStatusCode());
+        //      5. Çalışan isminin "Airi Satou" olduğunu ,
+        Assert.assertEquals(expectedDataMap.get("besincicalisan"),
+                ((Map)((List)actualDataMap.get("data")).get(4)).get("employee_name") );
+        //  çalışan sayısının 24 olduğunu,
+        Assert.assertEquals(expectedDataMap.get("calisansayisi"),
+                ((List) actualDataMap.get("data")).size());
+
+        //Sondan 2. çalışanın maaşının 106450 olduğunu
+        //önce actual datadan bize dönen listin size ini almalıyız
+
+        int dataSize=((List) actualDataMap.get("data")).size();
+
+        Assert.assertEquals(expectedDataMap.get("sondanikincicalisanmaasi"),
+                ((Map)((List<?>) actualDataMap.get("data")).get(dataSize-2)).get("employee_salary"));
+
+        //40,21 ve 19 yaslarında çalışanlar olup olmadığını
+        List<Integer> actualYas = new ArrayList<>();
+
+        for (int i = 0; i < dataSize; i++) {
+            actualYas.add((Integer) ((Map)((List) actualDataMap.get("data")).get(i)).get("employee_age"));
+        }
+
+        Assert.assertTrue(actualYas.containsAll((List) expectedDataMap.get("arananyaslar")));
+
+        //11. Çalışan bilgilerinin
+        //    {
+        // “id”:”11”
+        //        "employee_name": "Jena Gaines",
+        //            "employee_salary": "90560",
+        //            "employee_age": "30",
+        //            "profile_image": "" } oldugunu test edin.
+        Assert.assertEquals(((Map)(expectedDataMap.get("onbirincicalisan"))).get("employee_name"),
+                ((Map) ((List) actualDataMap.get("data")).get(10)).get("employee_name"));
+
+
+        Assert.assertEquals(((Map) expectedDataMap.get("onbirincicalisan")).get("employee_salary"),
+
+                ((Map) ((List) actualDataMap.get("data")).get(10)).get("employee_salary"));
+
+        Assert.assertEquals(((Map) expectedDataMap.get("onbirincicalisan")).get("employee_age"),
+                ((Map) ((List) actualDataMap.get("data")).get(10)).get("employee_age"));
+
+        Assert.assertEquals(((Map) expectedDataMap.get("onbirincicalisan")).get("profile_image"),
+                ((Map) ((List) actualDataMap.get("data")).get(10)).get("profile_image"));
+
+
 
     }
 
